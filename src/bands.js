f8da18e3e0524f5fb90afac014855d626cf69431
Fletch Padgett <fletch@flatanimals.org>
2017-04-07 08:37:04 -0400

    add Materialize bands and example

--- src/bands.js
+++ src/bands.js
@@ -37,6 +37,43 @@ const Bulma = {
   }
 };
 
+
+// Materialize (http://materializecss.com/)
+const Materialize = {
+  // Named Media Queries
+  mediumAndUp: '(min-width: 601px)',
+  largeAndUp: '(min-width: 993px)',
+  extraLargeAndUp: '(min-width: 1201px)',
+  smallAndDown: '(max-width: 600px)',
+  mediumAndDown: '(max-width: 992px) ',
+  mediumOnly: '(min-width: 601px) and (max-width: 992px)',
+
+  // Media Query Helpers mixin. Use globally or per component
+  mixin: {
+    computed: {
+      isMediumAndUp(){
+        return this.$resize && this.$mq.expr(this.$mq.bands.mediumAndUp);
+      },
+      isLargeAndUp(){
+        return this.$resize && this.$mq.expr(this.$mq.bands.largeAndUp);
+      },
+      isExtraLargeAndUp(){
+        return this.$resize && this.$mq.expr(this.$mq.bands.extraLargeAndUp);
+      },
+      isSmallAndDown(){
+        return this.$resize && this.$mq.expr(this.$mq.bands.smallAndDown);
+      },
+      isMediumAndDown(){
+        return this.$resize && this.$mq.expr(this.$mq.bands.mediumAndDown);
+      },
+      isMediumOnly(){
+        return this.$resize && this.$mq.expr(this.$mq.bands.mediumOnly);
+      },
+    }
+  }
+};
+
 export default {
-  Bulma
+  Bulma,
+  Materialize
 };
